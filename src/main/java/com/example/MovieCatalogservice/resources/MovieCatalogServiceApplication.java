e6559f7f83a026129281c1c507dfeed056fdf153
Admin <shwtjadhav@gmail.com>
2022-01-06 17:44:18 +0000

    Second Commit - Hello Docker

--- src/main/java/com/example/MovieCatalogservice/resources/MovieCatalogServiceApplication.java
+++ src/main/java/com/example/MovieCatalogservice/resources/MovieCatalogServiceApplication.java
@@ -2,26 +2,9 @@ package com.example.MovieCatalogservice.resources;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class MovieCatalogServiceApplication {
-
-	@Bean
-	public RestTemplate getRestTemplate(){
-	  return new RestTemplate();
-	}
-
-	@Bean
-	@LoadBalanced
-	public WebClient.Builder gerWebClient(){
-		return  WebClient.builder();
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogServiceApplication.class, args);
