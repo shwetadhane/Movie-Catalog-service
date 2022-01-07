2b3fd412c6d968ee0e686e0d656a57e8c9c5804b
Admin <shwtjadhav@gmail.com>
2022-01-07 06:18:34 +0000

    updated code for new Docker image

--- src/main/java/com/example/MovieCatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/com/example/MovieCatalogservice/resources/MovieCatalogResource.java
@@ -10,6 +10,6 @@ public class MovieCatalogResource {
      @RequestMapping("/")
     public String getCatalog(){
 
-         return "Welcome to Docker, with Latest build.. updated";
+         return "Welcome to Docker, with Latest build.. update Docker image";
     }
 }
