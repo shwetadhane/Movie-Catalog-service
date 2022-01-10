827db784aef01eafeebe15704adba60f546c8f8d
pradnyil <pradnyildhane@gmail.com>
2022-01-10 06:07:44 +0000

    Added Swagger, Spring cloud config, Service Registry and call through Api Gateway

--- src/main/java/com/example/MovieCatalogservice/resources/MovieCatalogServiceApplication.java
+++ src/main/java/com/example/MovieCatalogservice/resources/MovieCatalogServiceApplication.java
@@ -2,6 +2,8 @@ package com.example.MovieCatalogservice.resources;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
 
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
