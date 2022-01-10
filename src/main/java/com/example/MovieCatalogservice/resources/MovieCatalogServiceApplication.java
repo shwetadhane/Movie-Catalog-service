3111a74ccde0be22b375317768e1adb9b5761921
pradnyil <pradnyildhane@gmail.com>
2022-01-10 06:23:51 +0000

    Modified code post rebase

--- src/main/java/com/example/MovieCatalogservice/resources/MovieCatalogServiceApplication.java
+++ src/main/java/com/example/MovieCatalogservice/resources/MovieCatalogServiceApplication.java
@@ -2,10 +2,12 @@ package com.example.MovieCatalogservice.resources;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MovieCatalogServiceApplication {
 
 	public static void main(String[] args) {
